9e9cb5ba7c4e934742ec54375550c78a8646a322
Dan Lehner <lehnerdr@gmail.com>
2021-01-08 16:59:30 -0500

    finishes html for Register

--- app/src/pages/Register.js
+++ app/src/pages/Register.js
@@ -1,4 +1,4 @@
-import React from "react"; 
+import React, { useState } from "react"; 
 
 function Register(props) {
   return (
@@ -10,6 +10,59 @@ function Register(props) {
         }}>
           <h1 className="text-white mt-3 display-2">Register</h1>
           <h4 className="text-white" style={{ maxWidth: "300 px"}}>Already have an account with us? <a className="text-dark" style={{ backgroundColor: "#fafafa", padding: "2px" }} href="/login">Login!</a></h4>
+        </div>
+        <div className="col pl-4 border border-dark pt-3 pb-3">
+            <form>
+            <div className="form-group">
+              <label htmlFor="userName">Username</label>
+              <input 
+               type="text" 
+               className="form-control" 
+               name="username" 
+               />
+            </div>
+            <div className="form-group">
+            <label htmlFor="userName">Email Address</label>
+              <input 
+               type="email" 
+               className="form-control" 
+               name="email" 
+               />
+            </div>
+            <div className="form-group">
+            <label htmlFor="userName">Password</label>
+              <input 
+               type="password" 
+               className="form-control" 
+               name="password" 
+               />
+            </div>
+            <div className="form-group">
+            <label htmlFor="userName">Profile Pic URL</label>
+              <input 
+               type="text" 
+               className="form-control" 
+               name="profilePic" 
+               />
+            </div>
+            <div className="form-group">
+            <label htmlFor="userName">City</label>
+              <input 
+               type="text" 
+               className="form-control" 
+               name="city" 
+               />
+            </div>
+            <div className="form-group">
+            <label htmlFor="userName">Bio</label>
+              <input 
+               type="text" 
+               className="form-control" 
+               name="bio" 
+               />
+            </div>
+            <input className="btn btn-primary" type="submit" value="Create Account" />
+          </form>
         </div>
       </div>
     </div>
