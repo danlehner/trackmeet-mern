e4d28b2dfd0e7f12a34e189064fc064d9da6db87
Dan Lehner <lehnerdr@gmail.com>
2021-07-28 16:49:12 -0400

    start basic styling for login page

--- app/src/pages/Login.js
+++ app/src/pages/Login.js
@@ -27,40 +27,36 @@ const Login = (props) => {
   }
 
   return (
-    <div className="container show-container mt-5 mb-5">
-      <div className="row h-100" style={{ minHeight: 400 }}>
-        <div className="col" style={{
-          backgroundImage: `url("https://miro.medium.com/max/10368/1*o8tTGo3vsocTKnCUyz0wHA.jpeg")`, 
-          backgroundSize: "cover"
-        }}>
-          <h1 className="text-white mt-3 display-2">Login</h1>
-        </div>
-        <div className="col pl-4 border border-dark pt-3 pb-3">
-          {error && <p>{error}</p>}
-            <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input 
-               type="text" 
-               className="form-control" 
-               name="email" 
-               onChange={(e) => setEmail(e.target.value)}
-               value={email}
-               />
-            </div>
-            <div className="form-group">
-            <label htmlFor="userName">Password</label>
-              <input 
-               type="password" 
-               className="form-control" 
-               name="password" 
-               onChange={(e) => setPassword(e.target.value)}
-               value={password}
-               />
-            </div>
-            <input className="btn btn-primary" type="submit" value="Login" />
-          </form>
-        </div>
+    <div id="login-box" className="box large-box page-box dead-center">
+      <div className="col">
+        <h1 className="mega-text flush-text">Login</h1>
+        <img src="/assets/images/open.jpeg" />
+      </div>
+      <div className="col">
+        {error && <p>{error}</p>}
+          <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input 
+              type="text" 
+              className="form-control" 
+              name="email" 
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              />
+          </div>
+          <div className="form-group">
+          <label htmlFor="userName">Password</label>
+            <input 
+              type="password" 
+              className="form-control" 
+              name="password" 
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              />
+          </div>
+          <input className="btn btn-primary" type="submit" value="Login" />
+        </form>
       </div>
     </div>
   )
